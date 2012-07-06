1dac457c5f2a39c5ae88423770e901b54594c17d
Michael Hirshleifer <111mth@caltech.edu>
2012-07-06 00:03:22 +0000

    put in the 48 request source locations, and draw lines between them and each data center
    Each is currently animated with random thickness. One line is drawn for each (source, DC) pair.

--- src/edu/caltech/glb/svgmap/svgmap.scala
+++ src/edu/caltech/glb/svgmap/svgmap.scala
@@ -18,11 +18,16 @@ object Main {def main(args : Array[String]) = {
 	
 	val dcs : Seq[DataCenter] = {
 		/** From geo_capacity.m */
-		val datacenter_location : Seq[WorldPt] = List(WorldPt(37, -120), WorldPt(47, -120), WorldPt(44, -120), WorldPt(40, -90), WorldPt(31, -83), WorldPt(38, -78), WorldPt(31, -99), WorldPt(28, -81), WorldPt(35, -79), WorldPt(33, -81))
+		val datacenter_location : Seq[WorldPt] = List(
+			WorldPt(37, -120), WorldPt(47, -120), WorldPt(44, -120), WorldPt(40, -90), WorldPt(31, -83),
+			WorldPt(38, -78), WorldPt(31, -99), WorldPt(28, -81), WorldPt(35, -79), WorldPt(33, -81))
 		
 		// Raw traces
 		val (solar, wind) = {
-			def read_file(fname : String) = new opencsv.CSVReader(new InputStreamReader(new compress.compressors.CompressorStreamFactory createCompressorInputStream (ClassLoader getSystemResourceAsStream ("edu/caltech/glb/svgmap/indata/" + fname)))).readAll.asScala
+			def read_file(fname : String) = new opencsv.CSVReader(new InputStreamReader(
+				new compress.compressors.CompressorStreamFactory createCompressorInputStream
+					(ClassLoader getSystemResourceAsStream ("edu/caltech/glb/svgmap/indata/" + fname))
+			)).readAll.asScala
 			("solar_supply_week.csv.bz2", "wind_supply_week.csv.bz2") map read_file
 		}
 		
@@ -41,12 +46,23 @@ object Main {def main(args : Array[String]) = {
 		}}
 	}
 	
+	/** Also hard-coded for now. Values taken from <code>geo_capacity.m</code>. */
+	val client_locs : Seq[WorldPt] = List(WorldPt(32, -87), WorldPt(34, -119), WorldPt(35, -92), WorldPt(37, -120), WorldPt(39, -105), WorldPt(41, -73),
+		WorldPt(39, -76), WorldPt(28, -81), WorldPt(31, -83), WorldPt(44, -114), WorldPt(40, -89), WorldPt(40, -86),
+		WorldPt(41, -93), WorldPt(38, -98), WorldPt(38, -85), WorldPt(31, -92), WorldPt(45, -69), WorldPt(39, -77),
+		WorldPt(42, -72), WorldPt(43, -84), WorldPt(45, -93), WorldPt(33, -90), WorldPt(38, -92), WorldPt(47, -110),
+		WorldPt(41, -99), WorldPt(39, -116), WorldPt(43, -71), WorldPt(40, -74), WorldPt(34, -106), WorldPt(43, -76),
+		WorldPt(35, -79), WorldPt(47, -100), WorldPt(40, -83), WorldPt(35, -97), WorldPt(44, -120), WorldPt(41, -78),
+		WorldPt(42, -71), WorldPt(32, -80), WorldPt(44, -100), WorldPt(35, -86), WorldPt(31, -99), WorldPt(40, -112),
+		WorldPt(44, -73), WorldPt(38, -79), WorldPt(47, -121), WorldPt(39, -81), WorldPt(44, -89), WorldPt(43, -107))
+	
 	def randomLineStat = {
 		def randLine = LineState(math.random)
 		(GenSeq fill 20)(randLine).seq
 	}
-	val lines = List(
-		Line(WorldPt(40, -90), WorldPt(35, -100), randomLineStat)
-	)
+	// Draw 1 line for each (client, dc) pair
+	val lines = dcs flatMap {case DataCenter(dc_loc, _) ⇒
+		client_locs map (client_loc ⇒ Line(client_loc, dc_loc, randomLineStat))
+	}
 	System.out write generate_visualization(dcs, lines)
 }}
