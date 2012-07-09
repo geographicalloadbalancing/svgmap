09ac3b858c9809eb6a2589d419eb53d060141fbf
Michael Hirshleifer <111mth@caltech.edu>
2012-07-09 23:49:28 +0000

    minor fix to chromium script

--- src/edu/caltech/glb/svgmap/chromium.sh
+++ src/edu/caltech/glb/svgmap/chromium.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 # Workaround for Chromium bug (https://code.google.com/p/chromium/issues/detail?id=40787)
-chromium-browser --allow-file-access-from-files ./svgmap.xhtml &
+chromium-browser --allow-file-access-from-files "$(realpath ./svgmap.xhtml)" &
